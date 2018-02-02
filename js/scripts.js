d5ebc9ef9eb6cb5ccd0d936e5a40c9b70d01e1c9
puli <lisanne_pueschel@172-27-223-174.DYNAPOOL.NYU.EDU>
2018-02-01 21:17:16 -0500

    chris fixed my fails

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 
 //$(document).ready(function(){
 $("#hoverphoto").hover(function() {
-$('#hoverphoto').attr("src","https://cdn.vox-cdn.com/thumbor/BbXgy7tWCpRR948TFLbu_FNTJHo=/13x0:514x376/1200x800/filters:focal(13x0:514x376)/cdn.vox-cdn.com/uploads/chorus_image/image/48332701/2010_4_fake1.0.jpg");
+$('#hoverphoto').attr("src","//cdn.vox-cdn.com/thumbor/BbXgy7tWCpRR948TFLbu_FNTJHo=/13x0:514x376/1200x800/filters:focal(13x0:514x376)/cdn.vox-cdn.com/uploads/chorus_image/image/48332701/2010_4_fake1.0.jpg");
 }, function() {
-$( '#hoverphoto' ).attr("src","https://inhabitat.com/wp-content/blogs.dir/1/files/2013/05/1-Mysterious-Brooklyn-Heights-Brownstone-.jpg");
+$( '#hoverphoto' ).attr("src","//inhabitat.com/wp-content/blogs.dir/1/files/2013/05/1-Mysterious-Brooklyn-Heights-Brownstone-.jpg");
 });
 //adapted from https://css-tricks.com/forums/topic/how-to-change-image-on-hover-of-a-different-div-jquery/
 
 $('.revealbutton').click(function() {
-  $('#hoverphoto').attr("src", "http://static.messynessychic.com/wp-content/uploads/2013/01/googleparisfakehouse.jpg");
+  $('#hoverphoto').attr("src", "//static.messynessychic.com/wp-content/uploads/2013/01/googleparisfakehouse.jpg");
 });
 
 $('.revealbutton').click(function(){
@@ -22,7 +22,7 @@ $('.revealbutton').click(function(){
 });
 
 $('.revealbutton').click(function() {
-  $('#hoverphoto').attr("src", "https://media2.giphy.com/media/3oKIPnSvy7Ybi54MU0/200.webp#2-grid1");
+  $('#hoverphoto').attr("src", "//media2.giphy.com/media/3oKIPnSvy7Ybi54MU0/200.webp#2-grid1");
 });
 
 //https://stackoverflow.com/questions/19365386/jquery-change-button-text-on-click
